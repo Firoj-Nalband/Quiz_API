faa023bf3c7d285d86a70107a6b446667cd148bf
webwerks <you@example.com>
2017-09-19 21:03:01 +0530

    Created Serializers(Questionserializer and AnswerSerializer)

--- Quiz_API/forum/serializers.py
+++ Quiz_API/forum/serializers.py
@@ -1,9 +1,21 @@
 from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
+from forum.models import Question, Answer
 
 
 # Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('url', 'username', 'email', 'is_staff')
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = ('title', 'user_id')
+
+class AnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Answer
+        fields = ('body', 'question_id', 'user_id')
